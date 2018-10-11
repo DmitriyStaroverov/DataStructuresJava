c779042e7381b815c6d1b9108aa23818a11bbe18
Dmitriy Staroverov <star492@mail.ru>
2018-10-11 23:32:24 +0400

    1234

---
+++ src/lesson_2/Array.java
@@ -0,0 +1,19 @@
+package lesson_2;
+
+public interface Array {
+
+    int getElement(int index);
+
+    void setElement(int value, int index);
+
+    void addElement();
+
+    void display();
+
+    int getSize();
+
+    boolean deleteElement(int value);
+
+
+
+}
---
+++ src/lesson_2/ArrayImpl.java
@@ -0,0 +1,33 @@
+package lesson_2;
+
+public class ArrayImpl implements Array {
+    @Override
+    public int getElement(int index) {
+        return 0;
+    }
+
+    @Override
+    public void setElement(int value, int index) {
+
+    }
+
+    @Override
+    public void addElement() {
+
+    }
+
+    @Override
+    public void display() {
+
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public boolean deleteElement(int value) {
+        return false;
+    }
+}
